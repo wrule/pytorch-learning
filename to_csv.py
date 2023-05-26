3582c8dfe5b6b026ae0131c9c9523a2e4a87a478
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-26 18:46:26 +0800

    提交

---
+++ to_csv.py
@@ -0,0 +1,10 @@
+def main():
+  in_file = open('./data/btcusdt-1683364830172.json')
+  out_file = open('./data/btcusdt-1683364830172.csv', 'w')
+  for line in in_file.readlines():
+      line = line.strip().strip(',').strip('[').strip(']')
+      out_file.writelines(line, '\n')
+  out_file.close()
+  in_file.close()
+
+main()
