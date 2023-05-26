b037d00e8daad34f51fe32353d55b42455e57135
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-26 17:43:30 +0800

    提交代码

--- main.py
+++ main.py
@@ -3,5 +3,12 @@ import torch
 def main():
   x = torch.rand(5, 3)
   print(x)
+  f = open('./data/btcusdt-1683364830172.json')
+  print('read')
+  lines = f.read()
+  print(lines[0])
+  print('ok')
+  f.close()
+  input("Press Enter to continue...")
 
 main()
