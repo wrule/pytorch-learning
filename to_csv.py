9558c51b77f1437ba7692df4496c97923f7358c6
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-27 14:33:52 +0800

    提交代码

--- to_csv.py
+++ to_csv.py
@@ -4,8 +4,8 @@ def main():
   in_file = open(in_file_path)
   out_file = open(out_file_path, 'w')
   for line in in_file.readlines():
-      line = line.strip().strip(',').strip('[').strip(']')
-      out_file.writelines([line, '\n'])
+    line = line.strip().strip(',').strip('[').strip(']')
+    out_file.writelines([line, '\n'])
   out_file.close()
   in_file.close()
 
