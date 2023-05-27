752b447768be4efdad9849f19f322bcb3e242135
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-27 14:32:21 +0800

    提交代码

--- to_csv.py
+++ to_csv.py
@@ -1,6 +1,8 @@
 def main():
-  in_file = open('./data/btcusdt-1683364830172.json')
-  out_file = open('./data/btcusdt-1683364830172.csv', 'w')
+  in_file_path = './data/btcusdt-1683364830172.json'
+  out_file_path = in_file_path.replace('.json', '.csv')
+  in_file = open(in_file_path)
+  out_file = open(out_file_path, 'w')
   for line in in_file.readlines():
       line = line.strip().strip(',').strip('[').strip(']')
       out_file.writelines([line, '\n'])
@@ -8,3 +10,4 @@ def main():
   in_file.close()
 
 main()
+# print('9999old1234'.replace('old', 'new'))
